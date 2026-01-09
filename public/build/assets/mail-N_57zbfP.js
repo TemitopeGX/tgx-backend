e98e062de7e407a5292878274f58d815320405d3
TemitopeGX <temitopegx@users.noreply.github.com>
2026-01-09 12:36:48 +0100

    Add missing slug column to resources table

---
+++ public/build/assets/mail-N_57zbfP.js
@@ -0,0 +1,1 @@
+import{a as o}from"./index-BfJnVgT_.js";const t=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"m12 5 7 7-7 7",key:"xquz4c"}]],e=o("ArrowRight",t);const a=[["rect",{width:"20",height:"16",x:"2",y:"4",rx:"2",key:"18n3k1"}],["path",{d:"m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7",key:"1ocrg3"}]],r=o("Mail",a);export{e as A,r as M};
